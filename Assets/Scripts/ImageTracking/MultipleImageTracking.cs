0cdb87f8002bcf3f46b959103ad7c6d6249e8d23
Christian Bertomeu <christianbertomeu04@gmail.com>
2026-03-23 13:09:16 +0100

    Vuelta de Fallas

--- Assets/Scripts/ImageTracking/MultipleImageTracking.cs
+++ Assets/Scripts/ImageTracking/MultipleImageTracking.cs
@@ -10,6 +10,7 @@ public class MultipleImageTracking : MonoBehaviour
     [SerializeField] List<GameObject> prefabsToSpawn = new List<GameObject>();
 
     [SerializeField] private ARTrackedImageManager m_TrackedImageManager;
+    [SerializeField] private ARAnchorManager m_AnchorManager;
 
     private Dictionary<string, GameObject> arObjects;
 
@@ -40,11 +41,12 @@ public class MultipleImageTracking : MonoBehaviour
         foreach (var newImage in eventArgs.added)
         {
             UpdateTrackedImages(newImage);
+            CreateAnchorAsync(newImage);
         }
 
         foreach (var updatedImage in eventArgs.updated)
         {
-            UpdateTrackedImages(updatedImage);
+            //UpdateTrackedImages(updatedImage);
         }
 
         foreach (var removedImage in eventArgs.removed)
@@ -62,10 +64,29 @@ public class MultipleImageTracking : MonoBehaviour
         //     return;
         // }
 
-        Debug.Log(trackedImage.referenceImage.name);
+        //Debug.Log(trackedImage.referenceImage.name);
 
         arObjects[trackedImage.referenceImage.name].gameObject.SetActive(true);
         arObjects[trackedImage.referenceImage.name].transform.position = trackedImage.transform.position;
         arObjects[trackedImage.referenceImage.name].transform.rotation = trackedImage.transform.rotation;
+
+        // if (arObjects[trackedImage.referenceImage.name].gameObject.GetComponent<ARAnchor>() == null)
+        // {
+        //     arObjects[trackedImage.referenceImage.name].gameObject.AddComponent<ARAnchor>();
+        // }
     } 
+
+    async void CreateAnchorAsync(ARTrackedImage trackedImage)
+    {
+        var pose = new Pose(trackedImage.transform.position, trackedImage.transform.rotation);
+        Debug.Log(pose);
+
+        var result = await m_AnchorManager.TryAddAnchorAsync(pose);
+        if (result.status.IsSuccess())
+        {
+            var anchor = result.value;
+            Debug.Log(anchor.GetType());
+            arObjects[trackedImage.referenceImage.name].transform.SetParent(anchor.transform);
+        }
+    }
 }
